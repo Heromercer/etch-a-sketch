076315a49d57b7da50f1499c554a1de9b3a333c3
Alec <150299829+Heromercer@users.noreply.github.com>
2025-05-14 15:37:03 -0400

    Add function to alter size of grid

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const sketchContainer = document.querySelector(".sketch-container");
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.style.border = "1px solid gray";
-    div.style.boxSizing = "border-box";
-    div.style.height = "40px";
-    div.style.width = "40px";
-    sketchContainer.appendChild(div);
-}
+setGridSize(2);
 
 const sketchSquare = sketchContainer.childNodes;
-console.log(typeof sketchSquare);
 sketchSquare.forEach((div) => {
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = "black";
     });
 });
+
+const sizeButton = document.querySelector("#size-button");
+sizeButton.addEventListener("click", () => {
+    let size = prompt("Enter grid size...");
+    let changedHeight = 640/Number(size);
+    let changedWidth = 640/Number(size);
+    sketchSquare.forEach((div) => {
+        div.style.backgroundColor = "white";
+        div.style.height = changedHeight + 'px';
+        div.style.width = changedWidth + 'px';
+    });
+});
+
+function setGridSize(size) {
+    let area = size * size;
+    let height = 640/size;
+    let width = 640/size;
+    for (let i = 0; i < area; i++) {
+        const div = document.createElement("div");
+        div.style.border = "1px solid gray";
+        div.style.boxSizing = "border-box";
+        div.style.height = height + 'px';
+        div.style.width = width + 'px'; 
+        sketchContainer.appendChild(div);
+    }
+}
