baf0d082b59d1193c38d502074c6f60809824e72
Alec <150299829+Heromercer@users.noreply.github.com>
2025-05-14 14:40:45 -0400

    Add drawing logic to child nodes of the container

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.style.border = "1px solid gray";
     div.style.boxSizing = "border-box";
-    div.style.marginBottom = "-10px"
     div.style.height = "40px";
     div.style.width = "40px";
     sketchContainer.appendChild(div);
 }
+
+const sketchSquare = sketchContainer.childNodes;
+console.log(typeof sketchSquare);
+sketchSquare.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "black";
+    });
+});
